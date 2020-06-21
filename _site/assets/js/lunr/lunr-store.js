fd65c7f5e18ed13f61565ac617ca0373f8836665
Aditya <supadhy6@jhu.edu>
2020-06-21 10:19:20 -0400

    Update build

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": ".Hallo world?",
         "excerpt":"I’m trying something new, shifting from vanilla html5up to Jekyll. This helps in a few areas: one is dynamic content updates, with the static blog-like format that Jekyll provides, along with a greater usage of markdown in the whole rendering process. My apologies if the website is less user-friendly for...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/hallo-world/",
+        "url": "http://localhost:4000/others/hallo-world/",
         "teaser": null
       },{
         "title": "Our poster at V-VSS 2020",
         "excerpt":"Our experience of passage of time varies with the situation that we are in. For example, we report time being slowed down in near death experiences. In the cognitive psychology literature, this phenomenon is referred to as the subjective expansion of time. I along with Ian Phillips &amp; Jon Flombaum...","categories": [],
         "tags": ["Subjective expansion of time","perception","memory","vision"],
-        "url": "http://localhost:4000/poster@VSS_2020/",
+        "url": "http://localhost:4000/others/poster@VSS_2020/",
         "teaser": null
       },{
         "title": "Our talk at V-VSS 2020",
         "excerpt":"I am excited to share my research on how space and time dissociate in the construction of a visual moment. This work - in collaboration with Ian Phillips and my advisor Jon Flombaum - suggests that a moment in our visual experience is a snapshot that is stitched together from...","categories": [],
         "tags": ["Visual moment","Differntial processing of Fovea & Periphery","RSVP","Awareness"],
-        "url": "http://localhost:4000/talk@VSS_2020/",
+        "url": "http://localhost:4000/others/talk@VSS_2020/",
         "teaser": null
       }]
